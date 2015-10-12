083767fbfad7460f0b95e673e581f197fa304c8c
Brian Kang <kang36897@gmail.com>
2015-10-12 21:56:36 +0800

    1. add feature guide pages

---
+++ app/src/main/java/com/gulu/onsight/view/ScrollVerticalLinearLayout.java
@@ -0,0 +1,25 @@
+package com.gulu.onsight.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by lulala on 6/10/15.
+ */
+public class ScrollVerticalLinearLayout extends LinearLayout {
+    public ScrollVerticalLinearLayout(Context context) {
+        super(context);
+    }
+
+    public ScrollVerticalLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollVerticalLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+}
